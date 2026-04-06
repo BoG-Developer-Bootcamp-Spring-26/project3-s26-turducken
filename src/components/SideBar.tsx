73042c3b4de3b0b27a1a19dfe858e390163f203e
Rachael Setia <rachaelsetia@gmail.com>
2026-04-06 15:33:23 -0400

    Linked all icons and labels

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,24 +1,27 @@
 import Image from "next/image";
+import Link from "next/link";
 import inactTrainingLog from "../../public/images/inactiveTrainingLogs.png";
 import inactAnimal from "../../public/images/inactiveAnimalLogo.png";
 import inactAllTraining from "../../public/images/inactiveAllTrainingLogo.png";
 import inactAllAnimals from "../../public/images/inactiveAllAnimalsLogo.png";
 import inactAllUsers from "../../public/images/inactiveAllUsersLogo.png";
-import actTrainingLog from "../../public/images/activeTrainingLogs.png";
-import actAnimal from "../../public/images/activeAnimalLogo.png";
+import actTrainingLog from "../../public/images/activeTrainingLogo.png";
+import actAnimal from "../../public/images/activeAnimalsLogo.png";
 import actAllTraining from "../../public/images/activeAllTrainingLogo.png";
 import actAllAnimals from "../../public/images/activeAllAnimalsLogo.png";
 import actAllUsers from "../../public/images/activeAllUsersLogo.png";
+import { useRouter } from "next/router";
+import { usePathname } from "next/navigation";
 
 interface SideBarProps {
     fullName: string,
-    isAdmin: boolean
+    admin: boolean
 }
 
 const NAV_ITEMS = [
     {
         label: "Training Logs",
-        href: "/context/dashboard/training-logs",       //FIXME: update href if needed
+        href: "/dashboard/training-logs",       //FIXME: update href if needed
         icon: {
             inactive: <Image src={inactTrainingLog} alt="Training Log Icon" />,
             active: <Image src={actTrainingLog} alt = "Training Log Icon" />
@@ -26,7 +29,7 @@ const NAV_ITEMS = [
     },
     {
         label: "Animals",
-        href: "/context/dashboard/animals",             //FIXME: update href if needed
+        href: "/dashboard/animals",             //FIXME: update href if needed
         icon: {
             inactive: <Image src={inactAnimal} alt="Animal Icon" />,
             active: <Image src={actAnimal} alt = "Animal Icon" />
@@ -37,7 +40,7 @@ const NAV_ITEMS = [
 const ADMIN_ITEMS = [
     {
         label: "All Training",
-        href: "/context/dashboard/all-training-logs",   //FIXME: same
+        href: "/admin/all-training-logs",       //FIXME: same
         icon: {
             inactive: <Image src={inactAllTraining} alt="All Training Logs Icon" />,
             active: <Image src={actAllTraining} alt="All Training Logs Icon" />
@@ -45,7 +48,7 @@ const ADMIN_ITEMS = [
     },
     {
         label: "All Animals",
-        href: "/context/dashboard/all-animals",         //FIXME: same
+        href: "/admin/all-animals",             //FIXME: same
         icon: {
             inactive: <Image src={inactAllAnimals} alt="All Animals Icon" />,
             active: <Image src={actAllAnimals} alt="All Animals Icon" />
@@ -53,7 +56,7 @@ const ADMIN_ITEMS = [
     },
     {
         label: "All Users",
-        href: "/context/dashboard/all-users",           //FIXME: same
+        href: "/admin/all-users",               //FIXME: same
         icon: {
             inactive: <Image src={inactAllUsers} alt="All Users Icon" />,
             active: <Image src={actAllUsers} alt="All Users Icon" />
@@ -63,11 +66,31 @@ const ADMIN_ITEMS = [
 
 export default function SideBar({
     fullName,
-    isAdmin
+    admin
 }: SideBarProps) {
+    const router = useRouter();
+    const pathname = usePathname();
+
     return (
         <div>
-
+            <nav>
+            {NAV_ITEMS.map((item) => {
+                    const active = pathname === item.href;
+                    return (
+                        <Link
+                            key={item.href}
+                            href={item.href}
+                            className={"flex"}
+                        >
+                            {active ? item.icon.active : item.icon.inactive}
+                            <span className={active ? "text-white font-medium" : "text-[#565252] font-normal"}>
+                                {item.label}
+                            </span>
+                        </Link>
+                    );
+                })
+            }
+            </nav>
         </div>
     )
 }
