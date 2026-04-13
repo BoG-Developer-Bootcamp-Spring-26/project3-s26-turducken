a9cdda018c479ec30d22beb000fdc5238dd298f3
LussierBr <blussier6@gatech.edu>
2026-04-13 11:52:27 -0400

    create delete button component and put it on training log card

--- src/components/TrainingCard.tsx
+++ src/components/TrainingCard.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import DeleteButton from './DeleteButton';
 
 interface TrainingCardProps {
     trainingLogId: string;
@@ -57,9 +58,12 @@ export default function TrainingCard({
             {description}
           </p>
         </div>
+        <div className='flex flex-col justify-between'>
         <button onClick={handleEditLog} className='w-14'>
           <Image src="/images/trainingLogcardEditButton.png" alt="Edit Training Log" width={80} height={80}/>
         </button>
+        <DeleteButton type='training' id={trainingLogId}/>
+        </div>
       </div>
     </div>
   );
